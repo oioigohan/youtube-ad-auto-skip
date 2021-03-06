e3570c7a9aaeb8524cd212351ab0477de13c3c78
oioigohan <oioigohan+github@gmail.com>
2021-03-07 04:47:14 +0900

    広告を自動でスキップする機能を追加
    メインの広告、サブ広告、アンケートなどの動画プレイヤー上に出てくる広告を自動でスキップします。

--- src/content.js
+++ src/content.js
@@ -1,1 +1,31 @@
-window.alert('hello world!');
+elem = (className) => document.getElementsByClassName(className)[0];
+
+player = () => elem("video-stream");
+btnSkip = () => elem("ytp-ad-skip-button");
+btnClose = () => elem("ytp-ad-overlay-close-button");
+
+function click(btn, message) {
+  btn.addEventListener("click", () => console.log(message));
+  btn.click();
+}
+
+const playerObserver = new MutationObserver(() => {
+  if (btnSkip())
+    click(btnSkip(), "[YouTube Ads Auto Skipper] Skipped a main Ad.");
+  if (btnClose())
+    click(btnClose(), "[YouTube Ads Auto Skipper] Closed a sub Ad.");
+});
+
+const config = {
+  attributes: true,
+  childList: true,
+  subtree: false,
+};
+
+window.addEventListener("load", () => {
+  playerObserver.observe(player(), config);
+});
+
+window.addEventListener("unload", () => {
+  playerObserver.disconnect();
+});
